37f52e31c6e46595bb850b974468d5a90478b6d8
Zekumi <zac.taieb@gmail.com>
2020-08-24 17:34:50 +0200

    adsdf

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true
   validates :encrypted_password, presence: true
   validates :birth_date, presence: true
   validates :birth_location, presence: true
