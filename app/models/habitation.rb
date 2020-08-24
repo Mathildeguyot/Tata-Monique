6b532d5dfca86ca18445e4a4a2ebe7388122675b
lea mousel <leamousel51@gmail.com>
2020-08-24 16:33:27 +0200

    add validations to habitation model

--- app/models/habitation.rb
+++ app/models/habitation.rb
@@ -1,3 +1,12 @@
 class Habitation < ApplicationRecord
   belongs_to :user
+
+  validates :number, numericality: { only_integer: true }
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :zip_code, format: { with: /\A[a-zA-Z]+\z/, message: "format invalide" }
+  validates :category, inclusion: { in: ["Propriétaire", "Locataire", "Sous-locataire", "En cité universitaire, résidence pour étudiant, foyer..."] }
+  validates :rent, numericality: true
+  validates :size, numericality: { only_integer: true }
+  validates :furnished, presence: true
 end
