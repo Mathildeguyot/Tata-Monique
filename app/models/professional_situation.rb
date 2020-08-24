0dee650d208b2c679a551345d28fab6438c443bd
Ines Delbecq <50133505+inesdelbecq@users.noreply.github.com>
2020-08-24 16:41:25 +0200

    Update professional_situation.rb

--- app/models/professional_situation.rb
+++ app/models/professional_situation.rb
@@ -3,6 +3,6 @@ class ProfessionalSituation < ApplicationRecord
   validates :income, presence: true
   validates :fiscal_number, format: { with: /^[0-9]{13}$/ }
   validates :contract, inclusion: { in: %w[CDI CDD stage freelance intermittent autoentrepreneur] }
-  validates :status, inclusion: { in: %w[étudiant "en avitivité" "sans activité professionnelle"] }
+  validates :status, inclusion: { in: %w[étudiant "en activité" "sans activité professionnelle"] }
   validetes :user, presence: true
 end
