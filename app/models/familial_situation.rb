03049ddace94ef9a3fac78b26fba3782056df4fa
Mathilde Guyot <mathilde.guyot1@gmail.com>
2020-08-24 16:38:38 +0200

    validations

--- app/models/familial_situation.rb
+++ app/models/familial_situation.rb
@@ -1,3 +1,6 @@
 class FamilialSituation < ApplicationRecord
   belongs_to :user
+  validates :children, presence: true
+  validates :relationship, inclusion: { in: %w[célibataire marié pacsé "en concubinage"] }
+  validates :user, presence: true
 end
--- app/models/professional_situation.rb
+++ app/models/professional_situation.rb
@@ -1,3 +1,8 @@
 class ProfessionalSituation < ApplicationRecord
   belongs_to :user
+  validates :income, presence: true
+  validates :fiscal_number, format: { with: /^[0-9]{13}$/ }
+  validates :contract, inclusion: { in: %w[CDI CDD stage freelance intermittent autoentrepreneur] }
+  validates :status, inclusion: { in: %w[étudiant "en avitivité" "sans activité professionnelle"] }
+  validetes :user, presence: true
 end
