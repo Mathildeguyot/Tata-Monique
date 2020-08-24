0030a6d3f9ff6a562eaa1f15fdb7371c1742d9cd
Zekumi <zac.taieb@gmail.com>
2020-08-24 16:43:44 +0200

    idemailregex

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ApplicationRecord
   has_one :professional_situation
   has_one :health
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :encrypted_password, presence: true
+  validates :birth_date, presence: true
+  validates :birth_location, presence: true
+  validates :ID_number, presence: true, format: { with: /^([0-9]{12})(X|V)$/ }
+  validates :nationality, presence: true
+  validates :username, presence: true
 end
