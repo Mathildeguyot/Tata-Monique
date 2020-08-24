6b29a9c18ccba59551fcf5bc192957a45dcc7735
Ines Delbecq <ines.delbecq@gmail.com>
2020-08-24 15:50:04 +0200

    created tables task subtask and usertask

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_131834) do
+ActiveRecord::Schema.define(version: 2020_08_24_134811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subtasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "deadline"
+    t.boolean "done"
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_subtasks_on_task_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.boolean "done"
+    t.date "deadline"
+    t.string "organization"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +48,13 @@ ActiveRecord::Schema.define(version: 2020_08_24_131834) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "usertasks", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_usertasks_on_task_id"
+  end
+
+  add_foreign_key "subtasks", "tasks"
+  add_foreign_key "usertasks", "tasks"
 end
