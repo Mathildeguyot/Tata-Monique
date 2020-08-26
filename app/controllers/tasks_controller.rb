c2155366c68aac81d97d5d08f9d6e333e776c4ee
Ines Delbecq <ines.delbecq@gmail.com>
2020-08-26 16:28:13 +0200

    deleted subtask controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,21 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
   def index
     @tasks = Task.all
     @subtasks = Subtask.all
+  end
+
+  def show
+    @subtask.task = @task
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :deadline, :organization)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :documents
-  resources :tasks, only: [:index] do
+  resources :tasks, only: [:index,:show] do
     resources :subtask, only: [:index, :edit, :update]
   end
 end
