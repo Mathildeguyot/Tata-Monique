e11ed280d5c58a30c6252d7e1c44d93ba0082d7a
Mathilde Guyot <mathilde.guyot1@gmail.com>
2020-08-27 15:46:46 +0200

    improving chrono

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -3,4 +3,11 @@ class Subtask < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :deadline, presence: true
+
+  def next_is_done?
+    subtasks = Subtask.all
+    subtask_index = subtasks.index(self)
+    next_subtask = subtasks[subtask_index + 1]
+    next_subtask.done if next_subtask
+  end
 end
