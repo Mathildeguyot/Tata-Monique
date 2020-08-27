9516536b53212b92a9c3f55bf4dc8bd085bbbdff
Mathilde Guyot <mathilde.guyot1@gmail.com>
2020-08-27 13:00:44 +0200

    adding lines and hover

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,11 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :deadline, presence: true
   validates :organization, presence: true
+
+  def next_is_done?
+    tasks = Task.all
+    task_index = tasks.index(self)
+    next_task = tasks[task_index + 1]
+    next_task.done if next_task
+  end
 end
