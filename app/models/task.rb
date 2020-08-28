7a90f52cc46d3dac480a48c343df08310430436e
Mathilde Guyot <mathilde.guyot1@gmail.com>
2020-08-28 15:08:20 +0200

    still improving

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,5 +7,18 @@ class Task < ApplicationRecord
   validates :deadline, presence: true
   validates :organization, presence: true
 
+  def next_is_done?(user)
+    tasks = Task.all
+    task_index = tasks.index(self)
+    next_task = tasks[task_index + 1]
+    next_task.done_by_user?(user) if next_task
+  end
 
+  def done_by_user?(user)
+    self.usertasks.find_by(user: user).done?
+  end
+
+  def user_task(user)
+    Usertask.find_by(user: user, task: self)
+  end
 end
