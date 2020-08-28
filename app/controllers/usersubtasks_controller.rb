aba7c67b0c04d4e9c03d85e3eff5e896a92bb94d
Ines Delbecq <ines.delbecq@gmail.com>
2020-08-28 12:19:03 +0200

    upload ok

--- app/controllers/usersubtasks_controller.rb
+++ app/controllers/usersubtasks_controller.rb
@@ -1,7 +1,23 @@
 class UsersubtasksController < ApplicationController
-  def edit
-  end
+  before_action :set_usersubtask
 
   def update
+    document = Document.new(usersubtask_params[:document])
+    document.user = @usersubtask.user
+    if document.save
+      @usersubtask.done = true
+      @usersubtask.save
+      redirect_to task_subtasks_path(@usersubtask.subtask.task)
+    end
+  end
+
+  private
+
+  def set_usersubtask
+    @usersubtask = Usersubtask.find(params[:id])
+  end
+
+  def usersubtask_params
+    params.require(:usersubtask).permit(document: [:category, :name, :photo])
   end
 end
