283b10aeca42960235be09254511b93ab89ffe64
lea mousel <leamousel51@gmail.com>
2020-09-03 17:36:54 +0200

    add www. to API link

--- app/controllers/subtasks_controller.rb
+++ app/controllers/subtasks_controller.rb
@@ -7,7 +7,7 @@ class SubtasksController < ApplicationController
 
     @user = current_user
 
-    url = "https://tatamonique.live/api/v1/users/#{@user.id}/usersubtasks/#{Usersubtask.all.order(:id)[3].id}"
+    url = "https://www.tatamonique.live/api/v1/users/#{@user.id}/usersubtasks/#{Usersubtask.all.order(:id)[3].id}"
 
     usersubtask_serialized = open(url).read
     usersubtask = Usersubtask.find(Usersubtask.all.order(:id)[3].id)
